c2cc38a1feb6c6873c300e682325348ff4f97896
MariomcgeeArt <mariomcgeeart@gmail.com>
2020-12-07 23:36:38 -0600

    updated

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,17 +1,26 @@
 // src/Project.js
 
-import React from 'react'
-import logo from './logo.svg'
+import React from 'react';
+import logo from './logo.svg';
 import './POPOSSpace.css';
+import { Link } from 'react-router-dom';
 
 function POPOSSpace(props) {
-  const { name, image, address, hours } = props
+  const { name, image, address, hours, id } = props
   return (
     <div className="POPOSSpace">
       <h1>{name}</h1>
       <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
       <div>{address}</div>
       <div>{hours}</div>
+      <Link to={`/details/${id}`}>
+  <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
+</Link>
+<h1>
+  <Link to={`/details/${id}`}>
+    {name}
+  </Link>
+</h1>
     </div>
   )
 }
